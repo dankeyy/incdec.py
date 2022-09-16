e85edc979e0f5328424b4a48f4ec28a29f70c5bd
dankeyy <dankeyy@protonmail.com>
2022-09-16 18:20:08 +0300

    got some tests in

---
+++ tester.py
@@ -0,0 +1,13 @@
+#coding=foo
+
+assert b"a++ ++a a-- --a".decode('foo') == "((a, a := a+1)[0]) ((a, a := a+1)[1]) ((a, a := a-1)[0]) ((a, a := a-1)[1])"
+assert b"'a++ ++a a-- --a'".decode('foo') == "'a++ ++a a-- --a'"
+assert b'"a++ ++a a-- --a"'.decode('foo') == '"a++ ++a a-- --a"'
+
+i = 6
+assert i-- == 6
+assert i == 5
+assert ++i == 6
+assert --i == 5
+assert i++ == 5
+assert i == 6
